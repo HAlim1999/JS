f9b013904151612f1063649e7dfd340c91ad082a
HAlim1999 <herrera.amores@gmail.com>
2022-05-08 22:56:41 -0300

    simulador

--- prestamo.js
+++ prestamo.js
@@ -1,13 +1,14 @@
 let capturar = function () {
     let importe= document.getElementById("importe").value;
     let sistema= document.getElementById("sistema").value;
+    let plazo = document.getElementById("plazo").value;
 
 
-    capital= importe
-    i=(0.165*30)/365
-    n=5
-    cuota = (capital*i)/(1-(1/((1+i)**n)))
-    t=cuota-(capital*i)
+    let capital= importe
+    let i=(0.165*30)/365
+    let n=plazo
+    let cuota = (capital*i)/(1-(1/((1+i)**n)))
+    let t=cuota-(capital*i)
 
     if(capital<=0){
         alert("No ingreso ningun importe o el mismo no es válido")
@@ -17,32 +18,37 @@ let capturar = function () {
 
         case "Fances":
             alert("La simulacion se realizara por la suma de "+importe+" calculado segun el sistema de amortizacion "+sistema)
+
+            let j = 1;
+
             for(j=1;j<=n;j++){
 
-                cuotaAmort=t*((1+i)**(j-1))
-                cuotaInteres=cuota-cuotaAmort
-                saldo = capital-t*((((1+i)**j)-1)/i)
+                let cuotaAmort=t*((1+i)**(j-1))
+                let cuotaInteres=cuota-cuotaAmort
+                let saldo = capital-t*((((1+i)**j)-1)/i)
 
-                insertarFila(j);
-                insertarFila(cuotaAmort);
-                insertarFila(cuotaInteres);
-                insertarFila(saldo);
+                let cicloArray = [null, j, cuotaAmort, cuotaInteres, saldo]
+                let tbDatos = document.getElementById('tablaMarcha').insertRow(-1)
+                for (let d = 1; d <= 4; d++) {
+                    insertarCelda(tbDatos, cicloArray[d])
+                }
+            }
 
-
-                console.log(cuota)
-                console.log(cuotaAmort)
-                console.log(cuotaInteres)
-                console.log(saldo)}
             break;
 
         /*SISTEMA ALEMAN*/
         case"Aleman":
         alert("La simulacion se realizara por la suma de "+importe+" calculado segun el sistema de amortizacion "+sistema)
-            cuotaAleman = capital/n
+            let cuotaAleman = capital/n
             for(k=0;k<n;k++){
-                saldo = capital-(cuotaAleman*k)
-                cuotaInt = saldo*i
+                let saldo = capital-(cuotaAleman*k)
+                let cuotaInt = saldo*i
 
+                let cicloArray = [null, k, cuotaAleman, cuotaInt, saldo]
+                let tbDatos = document.getElementById('tablaMarcha').insertRow(-1)
+                for (let d = 1; d <= 4; d++) {
+                    insertarCelda(tbDatos, cicloArray[d])
+                }
                 console.log(saldo)
                 console.log(cuotaInt)
             }
@@ -50,11 +56,28 @@ let capturar = function () {
         /*SISTEMA AMERICANO*/
         case "Americano":
             alert("La simulacion se realizara por la suma de "+importe+" calculado segun el sistema de amortizacion "+sistema)
-            cuotaAme = capital*i
+            let cuotaAme = capital*i
             for(g=1;g<=n;g++){
-                if(g==5){
-                    console.log(capital*(1+i));
+                if(g==n){
+                    let cuotaEnd = capital*(1+i)
+
+                    let cicloArray = [null, g, capital, cuotaAme, 0]
+                    let tbDatos = document.getElementById('tablaMarcha').insertRow(-1)
+                    for (let d = 1; d <= 4; d++) {
+                        insertarCelda(tbDatos, cicloArray[d])
+                    }
+
+
+                    console.log(cuotaEnd);
                 } else{
+
+                    let cicloArray = [null, g, 0, cuotaAme, capital]
+                    let tbDatos = document.getElementById('tablaMarcha').insertRow(-1)
+                    for (let d = 1; d <= 4; d++) {
+                        insertarCelda(tbDatos, cicloArray[d])
+                    }
+
+
                     console.log(cuotaAme);}
                 }
             break;
@@ -62,16 +85,11 @@ let capturar = function () {
 }
 }
 
-function insertarFila(){
-    let tbDatos = document.getElementById("tablaMarcha").insertRow(-1);
-    let col1 = tbDatos.insertCell(-1);
-    let col2 = tbDatos.insertCell(-1);
-    let col3 = tbDatos.insertCell(-1);
-    let col4 = tbDatos.insertCell(-1);
 
-    col1.innertHTML = "sdsdsd";
-    col2.innertHTML = "Nueva Celda";
-    col3.innertHTML = "Nueva Celda";
-    col4.innertHTML = "Nueva Celda";
 
-}
+function insertarCelda(row, content) {
+    stringContent = toString(content)
+    let col = row.insertCell(-1)
+    let newText = document.createTextNode(content)
+    col.appendChild(newText)
+  }
